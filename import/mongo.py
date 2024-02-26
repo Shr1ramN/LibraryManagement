ef784d0dd7cc76f9f2fa9c568febe7cb7420af2d
ShriramNarayana <coderslayerr@gmail.com>
2024-02-26 14:33:55 +0530

    Deleted

--- import/mongo.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-class MongoDBConnection:
-    def __init__(self, host, port):
-        self.client = MongoClient(host, port)
-        self.db = self.client["libfinal"]
-        self.books_collection = self.db["books"]
-        self.customers_collection = self.db["customers"]
-
